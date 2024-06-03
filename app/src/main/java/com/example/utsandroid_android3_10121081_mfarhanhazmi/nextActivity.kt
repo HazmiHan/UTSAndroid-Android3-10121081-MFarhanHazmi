a522cfe0c052a7bec7acc3e92a2a2c190cfbd9d6
M Farhan Hazmi <farhanhazmi29@gmail.com>
2024-06-03 21:22:19 +0700

    First commit

---
+++ app/src/main/java/com/example/utsandroid_android3_10121081_mfarhanhazmi/nextActivity.kt
@@ -0,0 +1,10 @@
+package com.example.utsandroid_android3_10121081_mfarhanhazmi
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cek_isian_data)
+    }
+}
